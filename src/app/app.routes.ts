d1ff632c284833657e74095ef759b5f9c0d31d1c
blurfibers <Bluefibersltd@gmail.com>
2026-09-01 22:18:34 +0800

    contact page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Textile } from './pages/textile/textile';
 import { NonTextile } from './pages/non-textile/non-textile';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Blue Fibers | Home' },
   { path: 'textile', component: Textile, title: 'Blue Fibers | Textile' },
   { path: 'non-textile', component: NonTextile, title: 'Blue Fibers | Non-Textile' },
+  { path: 'contact', component: Contact, title: 'Blue Fibers | Contact' },
   { path: '**', redirectTo: '' },
 ];
